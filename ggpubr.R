b35d5f8d2a66795c1ae0a431b86b47c95b5d62d3
Win Khaing <dr.winkhaing@gmail.com>
2024-07-29 09:22:16 +0800

    update on 29 July

---
+++ ggpubr.R
@@ -0,0 +1,49 @@
+library(ggpubr)
+#> Loading required package: ggplot2
+# Create some data format
+# :::::::::::::::::::::::::::::::::::::::::::::::::::
+set.seed(1234)
+wdata = data.frame(
+  sex = factor(rep(c("F", "M"), each=200)),
+  weight = c(rnorm(200, 55), rnorm(200, 58)))
+head(wdata, 4)
+#>   sex   weight
+#> 1   F 53.79293
+#> 2   F 55.27743
+#> 3   F 56.08444
+#> 4   F 52.65430
+
+# Density plot with mean lines and marginal rug
+# :::::::::::::::::::::::::::::::::::::::::::::::::::
+# Change outline and fill colors by groups ("sex")
+# Use custom palette
+ggdensity(wdata, x = "weight",
+          add = "mean", rug = TRUE,
+          color = "sex", fill = "sex",
+          palette = c("#00AFBB", "#E7B800"))
+
+
+# Load data
+data("ToothGrowth")
+df <- ToothGrowth
+head(df, 4)
+#>    len supp dose
+#> 1  4.2   VC  0.5
+#> 2 11.5   VC  0.5
+#> 3  7.3   VC  0.5
+#> 4  5.8   VC  0.5
+
+# Box plots with jittered points
+# :::::::::::::::::::::::::::::::::::::::::::::::::::
+# Change outline colors by groups: dose
+# Use custom color palette
+# Add jitter points and change the shape by groups
+p <- ggboxplot(df, x = "dose", y = "len",
+               color = "dose", palette =c("#00AFBB", "#E7B800", "#FC4E07"),
+               add = "jitter", shape = "dose")
+p
+# Add p-values comparing groups
+# Specify the comparisons you want
+my_comparisons <- list( c("0.5", "1"), c("1", "2"), c("0.5", "2") )
+p + stat_compare_means(comparisons = my_comparisons)+ # Add pairwise comparisons p-value
+  stat_compare_means(label.y = 50)                   # Add global p-value
